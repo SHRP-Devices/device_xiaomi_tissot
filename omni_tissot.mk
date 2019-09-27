bf7e8db5d43a84e20057db641fe021250e530570
Giovanni Gualtieri <ggualtierone@gmail.com>
2019-09-27 18:04:52 +0200

    tissot: disable AB ota postinstall [1/2] + update fstab
    This prevents the error 5 on 2nd step while flashing roms.

--- omni_tissot.mk
+++ omni_tissot.mk
@@ -22,19 +22,6 @@ $(call inherit-product, build/target/product/embedded.mk)
 # A/B
 AB_OTA_UPDATER := true
 
-AB_OTA_PARTITIONS += \
-    boot \
-    system
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script
-
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
